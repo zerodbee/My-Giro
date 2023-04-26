b00779454ca0b497f020037f5feec8a06deeb330
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-04-26 22:08:59 +0300

    Добавил текстовое описание каждого изображения, и немножко подправил JS-код.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let items = document.querySelectorAll('.carousel .carousel-item')
 items.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
+    for (let i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
             next = items[0]
